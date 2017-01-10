e8da4befe95da9cd96c4d624f84c1524d095ed02
Paul Madden <maddenp@colorado.edu>
2017-01-10 11:16:08 -0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,7 +4,9 @@ conda update conda-build
 conda build .
 linuxpkg=$(conda build . --output)
 anaconda upload --force -u catees $linuxpkg
-conda convert -p osx-64 $linuxpkg
-osxpkg=osx-64/$(basename $linuxpkg)
-anaconda upload --force -u catees $osxpkg
-rm -rf ./osx-64
+for arch in osx-64; do
+  conda convert -p $arch $linuxpkg
+  archpkg=$arch/$(basename $linuxpkg)
+  anaconda upload --force -u catees $archpkg
+  rm -rf ./$arch
+done
